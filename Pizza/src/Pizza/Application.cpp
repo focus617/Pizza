29c172970ac33a8cb098afe0bb3bbb5112bd07d8
Xu Zhiyong <13980668829@163.com>
2022-04-15 20:33:47 +0800

    Dispatching events from GLFW.

--- Pizza/src/Pizza/Application.cpp
+++ Pizza/src/Pizza/Application.cpp
@@ -7,14 +7,25 @@
 #include <GLFW/glfw3.h>
 
 namespace Pizza {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		PZ_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -27,5 +38,10 @@ namespace Pizza {
 		}
 	}
 
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 
 }
