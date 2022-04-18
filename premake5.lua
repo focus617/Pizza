846804683190860031b43371f87cc543dc374d57
Xu Zhiyong <13980668829@163.com>
2022-04-18 12:45:29 +0800

    defined startproject in premake.

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,14 @@
 workspace "Pizza"
     architecture "x64"
+    startproject "Sandbox"
 
-    configurations { "Debug", "Release", "Dist"}
+    configurations { 
+        "Debug",
+        "Release",
+        "Dist"
+    }
 
     outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
 
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
