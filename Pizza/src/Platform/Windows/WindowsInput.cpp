582518310ebebf46010f75b59e8491954e8fe8a8
Xu Zhiyong <13980668829@163.com>
2022-04-18 15:46:23 +0800

    Added input polling.

---
+++ Pizza/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,49 @@
+#include "pzpch.h"
+#include "WindowsInput.h"
+
+#include "Pizza/Application.h"
+#include <GLFW/glfw3.h>
+
+namespace Pizza {
+
+	Input* Input::s_Instance = new WindowsInput();
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		// glfwGetKey用于找到给定window中，最后一次输入的指定Key的状态
+		auto state = glfwGetKey(window, keycode);
+		// 判断是否是按下或重复状态；状态包含三种，松开，按下，重复。
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		// 获取给定window中鼠标的position
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		return { (float)xpos, (float)ypos };
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto [x, y] = GetMousePositionImpl();
+		return x;
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto [x, y] = GetMousePositionImpl();
+		return y;
+	}
+
+}
