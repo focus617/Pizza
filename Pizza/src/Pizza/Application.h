ca8da8d9d14fe349224cf4058dfb674077718625
Xu Zhiyong <13980668829@163.com>
2022-04-20 12:02:54 +0800

    Our first shader.

--- Pizza/src/Pizza/Application.h
+++ Pizza/src/Pizza/Application.h
@@ -9,6 +9,8 @@
 
 #include "Pizza/ImGui/ImGuiLayer.h"
 
+#include "Pizza/Renderer/Shader.h"
+
 namespace Pizza {
 
 	class PIZZA_API Application
@@ -35,6 +37,9 @@ namespace Pizza {
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
