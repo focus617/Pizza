80d54d1b8f749fa84a94f786f19bcf34424667ca
Xu Zhiyong <13980668829@163.com>
2022-04-23 22:40:15 +0800

    Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Pizza.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 class ExampleLayer : public Pizza::Layer
@@ -89,9 +92,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Pizza::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Pizza::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -108,20 +111,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			
+			uniform vec3 u_Color;
 
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Pizza::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Pizza::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 
@@ -152,13 +157,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Pizza::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Pizza::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Pizza::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Pizza::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -169,7 +177,9 @@ public:
 
     virtual void OnImGuiRender() override
     {
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
     }
 
     void OnEvent(Pizza::Event& event) override
@@ -180,7 +190,7 @@ private:
     std::shared_ptr<Pizza::Shader> m_Shader;
     std::shared_ptr<Pizza::VertexArray> m_VertexArray;
 
-    std::shared_ptr<Pizza::Shader> m_BlueShader;
+    std::shared_ptr<Pizza::Shader> m_FlatColorShader;
     std::shared_ptr<Pizza::VertexArray> m_SquareVA;
 
     Pizza::OrthographicCamera m_Camera;
@@ -189,6 +199,8 @@ private:
 
     float m_CameraRotation = 0.0f;
     float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Pizza::Application
