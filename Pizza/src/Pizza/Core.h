d7f9692e0966b8fd85ebb5351aebc491e6ed4818
Xu Zhiyong <13980668829@163.com>
2022-04-24 09:23:45 +0800

    Added Pizzal::Scope and Pizza::Ref definitions

--- Pizza/src/Pizza/Core.h
+++ Pizza/src/Pizza/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef PZ_PLATFORM_WINDOWS
 	#if PZ_DYNAMIC_LINK
 		#ifdef PZ_BUILD_DLL
@@ -31,3 +33,13 @@
 #define BIT(x) (1 << x)
 
 #define PZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Pizza {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
