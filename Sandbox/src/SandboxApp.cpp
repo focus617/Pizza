4f7e7f5fd843ee4c7126997b6922b749a5ecfe26
Xu Zhiyong <13980668829@163.com>
2022-04-25 00:01:44 +0800

    Added support for blending, and loading both RGB and RGBA textures

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -168,6 +168,7 @@ public:
 		m_TextureShader.reset(Pizza::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 
 		m_Texture = Pizza::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_ChernoLogoTexture = Pizza::Texture2D::Create("assets/textures/ChernoLogo.png");
 
 		std::dynamic_pointer_cast<Pizza::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Pizza::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -217,6 +218,9 @@ public:
 
 		m_Texture->Bind();
 		Pizza::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		m_ChernoLogoTexture->Bind();
+		Pizza::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
 
 		// Triangle
 		// Pizza::Renderer::Submit(m_Shader, m_VertexArray);
@@ -243,7 +247,7 @@ private:
     Pizza::Ref<Pizza::Shader> m_FlatColorShader, m_TextureShader;
     Pizza::Ref<Pizza::VertexArray> m_SquareVA;
 
-	Pizza::Ref<Pizza::Texture2D> m_Texture;
+	Pizza::Ref<Pizza::Texture2D> m_Texture, m_ChernoLogoTexture;
 
     Pizza::OrthographicCamera m_Camera;
     glm::vec3 m_CameraPosition;
