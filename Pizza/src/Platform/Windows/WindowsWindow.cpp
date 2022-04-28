bb1a84e0122ccfc1fa11795df0f768b39ae92af4
Xu Zhiyong <13980668829@163.com>
2022-04-28 09:42:54 +0800

    Basic ref-counting system to terminate glfw

--- Pizza/src/Platform/Windows/WindowsWindow.cpp
+++ Pizza/src/Platform/Windows/WindowsWindow.cpp
@@ -10,7 +10,7 @@
 namespace Pizza {
 
     // To ensure that GLFW only can be initialized once
-    static bool s_GLFWInitialized = false;
+    static uint8_t s_GLFWWindowCount = 0;
 
     static void GLFWErrorCallback(int error, const char* description)
     {
@@ -42,17 +42,19 @@ namespace Pizza {
         PZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
         // Make sure to initialize GLFW only once, along with the first window
-        if (!s_GLFWInitialized)
+        if (s_GLFWWindowCount == 0)
         {
+            PZ_CORE_INFO("Initializing GLFW");
+
             // TODO: glfwTerminate on system shutdown
             int success = glfwInit();
             PZ_CORE_ASSERT(success, "Could not intialize GLFW!");
             glfwSetErrorCallback(GLFWErrorCallback);
-            s_GLFWInitialized = true;
         }
 
         m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-       
+        ++s_GLFWWindowCount;
+
         m_Context = CreateScope<OpenGLContext>(m_Window);
 
         m_Context->Init();
@@ -149,6 +151,12 @@ namespace Pizza {
     void WindowsWindow::Shutdown()
     {
         glfwDestroyWindow(m_Window);
+
+        if (--s_GLFWWindowCount == 0)
+        {
+            PZ_CORE_INFO("Terminating GLFW");
+            glfwTerminate();
+        }
     }
 
     void WindowsWindow::OnUpdate()
