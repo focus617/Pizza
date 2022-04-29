7cc0dd30f48dd1fd327b0ef39152e9fcd035d62a
Xu Zhiyong <13980668829@163.com>
2022-04-29 10:13:47 +0800

    Added file reading check

--- Pizza/src/Platform/OpenGL/OpenGLShader.cpp
+++ Pizza/src/Platform/OpenGL/OpenGLShader.cpp
@@ -53,11 +53,19 @@ namespace Pizza {
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
-;		}
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				PZ_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
+		}
 		else
 		{
 			PZ_CORE_ERROR("Could not open file '{0}'", filepath);
